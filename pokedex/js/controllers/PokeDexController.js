1309209a4ecf45a8da7f938d94195e105fdbb93e
Fernando Santos <fernandop.santossa@gmail.com>
2023-07-20 15:50:24 -0300

    Projeto Ligando Views e Controllers

--- pokedex/js/controllers/PokeDexController.js
+++ pokedex/js/controllers/PokeDexController.js
@@ -1,19 +1,25 @@
 angular.module('pokedexApp')
 .controller('PokeDexController', PokeDexController);
 
-PokeDexController.$inject = ['PokeApiFactory']
+// PokeDexController.$inject = ['PokeApiFactory']
 
 function PokeDexController(PokeApiFactory){
     var vm = this;
     vm.searchText = '';
-    vm.pkmList = [];
+    vm.pkmList = [
+        {name: 'bulbasaur', number: '001'},
+        {name: 'pikachu', number: '025'},
+        {name: 'mew', number: '151'},
+        {name: 'chikorita', number: '152'},
+        {name: 'celebi', number: '251'},
+    ];
 
-    if(PokeApiFactory.pkmList.length){
-       vm.pkmList = PokeApiFactory.pkmList;
-    }else{
-        PokeApiFactory.listAll()
-            .then(pkmList => {
-                vm.pkmList = pkmList;
-            })
-    }
+    // if(PokeApiFactory.pkmList.length){
+    //    vm.pkmList = PokeApiFactory.pkmList;
+    // }else{
+    //     PokeApiFactory.listAll()
+    //         .then(pkmList => {
+    //             vm.pkmList = pkmList;
+    //         })
+    // }
 }
