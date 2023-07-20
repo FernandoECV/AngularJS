a4a7c2be961a7d6c1c8845de65db712380a50c1a
Fernando Santos <fernandop.santossa@gmail.com>
2023-07-20 13:39:45 -0300

    Conhecendo o scope do Angular

---
+++ simple/js/app.js
@@ -0,0 +1,17 @@
+angular.module('meuApp', [])
+
+.controller('meuController', ['$scope', '$interval', function($scope, $interval){
+    $scope.nome = "Treina Web";
+    $scope.numero = 0;
+
+    // $interval(function(){
+    //     $scope.numero++;
+    // }, 1000);
+
+    // setInterval(function(){
+    //     $scope.$apply(function(){
+    //         $scope.numero++;    
+    //     }); 
+    // }, 1000);
+
+}]);//
